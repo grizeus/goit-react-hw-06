0cde2ebd951fe798163f0e324d0d9acac6aea431
grizeus <32486511+grizeus@users.noreply.github.com>
2024-10-14 02:38:09 +0300

    [FEAT] add persistance to store (#3)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
 
-export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filters: filtersReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const reducers = combineReducers({
+  contacts: contactsReducer,
+  filters: filtersReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+    },
+  }),
+});
+
+export const persistor = persistStore(store);
