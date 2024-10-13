641f7bedff1ace76dd34d46ed007c62e6b9a33a9
grizeus <zaytsewww@gmail.com>
2024-10-14 01:59:35 +0300

    finalize switch useState to Redux

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,39 +1,13 @@
-// import css from './App.module.css'
-import { useEffect, useState } from "react";
 import ContactForm from "../ContactForm/ContactForm";
 import ContactList from "../ContactList/ContactList";
 import SearchBox from "../SearchBox/SearchBox";
-import initialContacts from "../../data/contacts.json";
 
 function App() {
-  // use nullish coalescing to check if the stored contacts exist
-  const [contacts, setContacts] = useState(() => {
-    return (
-      JSON.parse(window.localStorage.getItem("stored-contacts")) ??
-      initialContacts
-    );
-  });
-  const [search, setSearch] = useState("");
-
-  useEffect(() => {
-    window.localStorage.setItem("stored-contacts", JSON.stringify(contacts));
-  }, [contacts]);
-
-  const deleteContact = contactID => {
-    setContacts(prevContacts => {
-      return prevContacts.filter(contact => contact.id !== contactID);
-    });
-  };
-
-  const filteredContacts = contacts.filter(contact => {
-    return contact.name.toLowerCase().includes(search.toLowerCase());
-  });
-
   return (
     <div>
       <ContactForm />
-      <SearchBox value={search} onFilter={setSearch} />
-      <ContactList contacts={filteredContacts} handleDelete={deleteContact} />
+      <SearchBox />
+      <ContactList />
     </div>
   );
 }
